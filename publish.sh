d657e7bfd51fa4421b2dadb66a3a41a6d0ff41db
Jayden Chan <jayden@jayden.codes>
2025-06-13 11:25:08 -0600

    chore: prep for public release

--- publish.sh
+++ publish.sh
@@ -1,23 +1,24 @@
-#!/bin/zsh
+#!/usr/bin/env bash
+set -eou pipefail
 
-set -e
+next_version="$(git cliff --bumped-version | rg 'v(\d+\.\d+\.\d+)' --only-matching --replace='$1' --color=never)"
+echo "publishing version $next_version"
 
-# increment version
-if [ "$1" = "major" ]; then
-    jq '.version = (.version | split(".") | map(tonumber) | "\(.[0] + 1).\(.[1]).\(.[2])")' package.json > package.json.tmp
-elif [ "$1" = "minor" ]; then
-    jq '.version = (.version | split(".") | map(tonumber) | "\(.[0]).\(.[1] + 1).\(.[2])")' package.json > package.json.tmp
-else
-    jq '.version = (.version | split(".") | map(tonumber) | "\(.[0]).\(.[1]).\(.[2] + 1)")' package.json > package.json.tmp
-fi
+jq ".version = \"$next_version\"" package.json > package.json.tmp
+jq ".version = \"$next_version\"" package-lock.json > package-lock.tmp1.json
+jq ".packages.\"\".version = \"$next_version\"" package-lock.tmp1.json > package-lock.json.tmp
 
 mv package.json.tmp package.json
+mv package-lock.json.tmp package-lock.json
+rm package-lock.tmp1.json
 
-new_version=$(jq -r .version package.json)
+commit_msg="chore: publish version $next_version";
 
-npm i
+npm ci
 npm run build
-git add package.json package-lock.json dist
-git commit -v -m "chore: publish version $new_version"
-git tag -a -m "v$new_version" "v$new_version"
+
+git cliff --bump --with-commit="$commit_msg" -o CHANGELOG.md
+git add package.json package-lock.json dist CHANGELOG.md
+git commit -m "$commit_msg"
+git tag -a -m "v$next_version" "v$next_version"
 git push --follow-tags
